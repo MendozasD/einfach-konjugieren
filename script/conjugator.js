e5fc8adf04ade1396643264ed1d2eb3c12f38193
David Mendoza <dmendozasommer@gmail.com>
2024-01-10 11:45:14 +0100

    First commit

---
+++ script/conjugator.js
@@ -0,0 +1,47 @@
+import data from "/data/verbs_database.json";
+
+export function conjugator(inputVerb) {
+  const foundVerb = data.verbs.find((verb) => verb.infinitive === inputVerb);
+  if (foundVerb) {
+    let conjugatedBox = `<div id="conjugated_box">
+      <h1 id="infinitive">${foundVerb.infinitive}</h1>
+
+      <div class="conjugated_row">
+        <p class="pronoun_column">ich</p>
+        <p class="conjugated_column">${foundVerb.conjugations.ich}</p>
+      </div>
+
+      <div class="conjugated_row">
+        <p class="pronoun_column">du</p>
+        <p class="conjugated_column">${foundVerb.conjugations.du}</p>
+      </div>
+
+      <div class="conjugated_row">
+        <p class="pronoun_column">er/sie/es</p>
+        <p class="conjugated_column">${foundVerb.conjugations.er}</p>
+      </div>
+
+      <div class="conjugated_row">
+        <p class="pronoun_column">wir</p>
+        <p class="conjugated_column">${foundVerb.conjugations.wir}</p>
+      </div>
+
+      <div class="conjugated_row">
+        <p class="pronoun_column">ihr</p>
+        <p class="conjugated_column">${foundVerb.conjugations.ihr}</p>
+      </div>
+
+      <div class="conjugated_row">
+        <p class="pronoun_column">Sie</p>
+        <p class="conjugated_column">${foundVerb.conjugations.Sie}</p>
+      </div>
+
+      <p id="particletxt">${foundVerb.conjugations.particletxt}</p>
+    </div>`;
+    document.querySelector("#conjugator_result").innerHTML = conjugatedBox;
+  } else {
+    document.querySelector(
+      "#conjugator_result"
+    ).innerHTML = `Verb '${inputVerb}' nitch gefunden.`;
+  }
+}
---
+++ script/main.js
@@ -0,0 +1,31 @@
+import "/style/style.scss";
+import { conjugator } from "/script/conjugator.js";
+
+document.querySelector("#app").innerHTML = `
+  <div id="container">
+    <div id="conjugator">
+      <h1 class="title">Einfach Konjugieren</h1>
+      <section id="input_field">
+        <input type="text" id="verb_input" placeholder="Wort eingeben" />
+        <button id="search_btn">los</button>
+        </section>
+      <section id="conjugator_result">
+      </section>
+      <section class="center_parent">
+        <button id="save_btn" disabled="true" class="pan_font">Speichern</button>
+      </section>
+    </div>
+
+    <div id="conjugated_list">
+      <h1 class="title">Konjugierte Verben</h1>
+    </div>
+  </div>
+`;
+
+let verb = document.querySelector("#verb_input");
+const searchBtn = document.querySelector("#search_btn");
+
+searchBtn.addEventListener("click", () => {
+  conjugator(verb.value);
+  document.querySelector("#save_btn").disabled = false;
+});
