722594599a1a9b7740c9f84e490ad8581803c5be
David Mendoza <dmendozasommer@gmail.com>
2024-01-27 16:59:03 +0100

    Saving verbs was duplicating them, so the toLowerCase function is now also used inside the savebtn event listener

--- script/main.js
+++ script/main.js
@@ -10,9 +10,10 @@ document.querySelector("#app").innerHTML = `
       <h1 class="title">Einfach Konjugieren</h1>
       <section id="input_field">
         <input type="text" id="verb_input" placeholder="Wort eingeben" />
-        <button id="search_btn"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-      </svg>
+      <button id="search_btn">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+        </svg>
       </button>
         </section>
       <section id="conjugator_result">
@@ -41,7 +42,7 @@ searchBtn.addEventListener("click", () => {
 });
 
 saveBtn.addEventListener("click", () => {
-  const currentVerb = document.querySelector("#" + verb.value);
+  const currentVerb = document.querySelector("#" + verb.value.toLowerCase());
   if (currentVerb) {
     saveBtn.style.backgroundColor = "var(--red)";
     saveBtn.innerHTML = "Schon gespeichern";
