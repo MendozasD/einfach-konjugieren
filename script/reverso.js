83c8ee4f0b74c1a100842ce71b728fcb8255f091
David Mendoza <dmendozasommer@gmail.com>
2024-01-30 18:16:05 +0100

    Examplaes json file created to store reverso function that does not run because of CORS errors

--- script/reverso.js
+++ script/reverso.js
@@ -1,8 +1,33 @@
 import Reverso from "reverso-api";
+import fs from "fs";
 const reverso = new Reverso();
 
 export function getContext(verb) {
   reverso.getContext(verb, "german", "english").then((response) => {
-    console.log(response);
+    const data = {
+      examples: response.examples.slice(0, 10).map((example) => example.source),
+    };
+
+    // Write data to JSON file
+    fs.readFile("./data/examples.json", "utf8", (err, jsonString) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const jsonData = JSON.parse(jsonString);
+      jsonData.germanVerbs.infinitive = verb;
+      jsonData.germanVerbs.examples = data.examples.map((example) =>
+        example.toString()
+      );
+
+      fs.writeFile("./data/examples.json", JSON.stringify(jsonData), (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log("Data added to JSON file successfully!");
+      });
+    });
   });
 }
